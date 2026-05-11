c67d20200afe65b49d9aac1ded338f5ce8cd76a4
aldofurlani <furlani@ualberta.ca>
2026-05-11 14:19:43 -0600

    feat: configure E138 experiment hyperparameters to those listed in paper

--- experiments/E138-two-biome-large/foragax/ForagaxTwoBiomeLarge-v1/slurm_search.sh
+++ experiments/E138-two-biome-large/foragax/ForagaxTwoBiomeLarge-v1/slurm_search.sh
@@ -1,3 +1,3 @@
-python scripts/slurm.py --cluster clusters/vulcan-cpu-30m.json --runs 30 --entry src/continuing_main.py --force -e experiments/E138-two-biome-large/foragax/ForagaxTwoBiomeLarge-v1/Baselines/Random.json
-python scripts/slurm.py --cluster clusters/vulcan-cpu-30m.json --runs 30 --entry src/continuing_main.py --force -e experiments/E138-two-biome-large/foragax/ForagaxTwoBiomeLarge-v1/Baselines/Search-Nearest.json
-python scripts/slurm.py --cluster clusters/vulcan-cpu-1h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E138-two-biome-large/foragax/ForagaxTwoBiomeLarge-v1/Baselines/Search-Oracle.json
+python scripts/slurm.py --cluster clusters/vulcan-cpu.json --time=0:30:00 --runs 30 --entry src/continuing_main.py --force -e experiments/E138-two-biome-large/foragax/ForagaxTwoBiomeLarge-v1/Baselines/Random.json
+python scripts/slurm.py --cluster clusters/vulcan-cpu.json --time=1:00:00 --runs 30 --entry src/continuing_main.py --force -e experiments/E138-two-biome-large/foragax/ForagaxTwoBiomeLarge-v1/Baselines/Search-Nearest.json
+python scripts/slurm.py --cluster clusters/vulcan-cpu.json --time=3:00:00 --runs 30 --entry src/continuing_main.py --force -e experiments/E138-two-biome-large/foragax/ForagaxTwoBiomeLarge-v1/Baselines/Search-Oracle.json
