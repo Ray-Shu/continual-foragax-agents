3c9038adecdef803fce04da0f385d35fca54d19f
Ray-Shu <rayshu1015@gmail.com>
2026-06-12 08:52:21 -0600

    added rtu ppo 128 learning curves

---
+++ experiments/XN35/foragax-sweep/ForagaxSquareWaveTwoBiome-v11/slurm.sh
@@ -0,0 +1,6 @@
+for fov in 9;
+do
+    python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-32G.json --tasks 24 --time 03:00:00 --runs 10 --entry src/rtu_ppo.py --force -e experiments/XN35/foragax-sweep/ForagaxSquareWaveTwoBiome-v11/${fov}/PPO-RTU_LN_128_1.json
+    python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-32G.json --tasks 24 --time 03:00:00 --runs 10 --entry src/rtu_ppo.py --force -e experiments/XN35/foragax-sweep/ForagaxSquareWaveTwoBiome-v11/${fov}/PPO-RTU_LN_128_1-l2-init.json
+    python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-32G.json --tasks 24 --time 03:00:00 --runs 10 --entry src/rtu_ppo.py --force -e experiments/XN35/foragax-sweep/ForagaxSquareWaveTwoBiome-v11/${fov}/PPO-RTU_LN_128_1_crelu.json
+done
